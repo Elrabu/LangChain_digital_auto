d58b367215901654cc29a750093638e454c67806
Elias Busch <49643239+Elrabu@users.noreply.github.com>
2026-07-03 00:02:39 +0200

    Update wiper_graph.py

--- SmartWiperAgents/graph/wiper_graph.py
+++ SmartWiperAgents/graph/wiper_graph.py
@@ -4,28 +4,28 @@ from agents.supervisor import supervisor_node
 from agents.safety_agent import safety_node
 from agents.actuator_agent import actuator_node
  
-def route(state: WiperState) -> str:
-    return state["next_agent"]
+def route(state: WiperState) -> str: #routing function for conditional edges
+    return state["next_agent"] #return the current next node which is set by the supervisor
  
-def build_graph():
-    g = StateGraph(WiperState)
- 
+def build_graph(): #function to set up the LangGraph workflow
+    g = StateGraph(WiperState) #create a new StateGraph with WiperState as the state schema
+    
+    #add all the LangGraph nodes needed for the workflow
     g.add_node("supervisor",      supervisor_node)
     g.add_node("safety_agent",    safety_node)
     g.add_node("actuator_agent",  actuator_node)
- 
-    g.set_entry_point("supervisor")
-    g.add_conditional_edges("supervisor", route, {
+    
+    g.set_entry_point("supervisor") #set the supervisor node as the entry point
+    g.add_conditional_edges("supervisor", route, { #set up conditional edges based on the returned state values from the route variable
         "safety_agent":   "safety_agent",
         "actuator_agent": "actuator_agent",
         "END":            END,
     })
- 
-    # Nach Spezialisten zurück zum Supervisor (Feedback-Loop)
+    
+    #define concrete node order for safety and actuator agent to supervisor agent
     g.add_edge("safety_agent",   "supervisor")
     g.add_edge("actuator_agent", "supervisor")
  
-    return g.compile()
+    return g.compile() #compile LangGraph
 
-# Exported for LangGraph Studio`
-graph = build_graph()
+graph = build_graph() # Exported for LangGraph Studio
