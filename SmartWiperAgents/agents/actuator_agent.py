0ebbd5fc24b6f133f778d70c41aff6d7801a29ea
Elias Busch <49643239+Elrabu@users.noreply.github.com>
2026-07-05 22:56:07 +0200

    Update actuator_agent.py

--- SmartWiperAgents/agents/actuator_agent.py
+++ SmartWiperAgents/agents/actuator_agent.py
@@ -1,20 +1,20 @@
 from graph.state import WiperState
 
-_vehicle_ref = None # global reference to velocitas vehicle object
+vehicle_reference = None # global reference to velocitas vehicle object
  
 def set_vehicle(v): #setter function to enable an injection of the vehicle object from outside the file
-    global _vehicle_ref #access global variable
-    _vehicle_ref = v #save variable v into global variable
+    global vehicle_reference #access global variable
+    vehicle_reference = v #save variable v into global variable
  
 async def actuator_node(state: WiperState) -> WiperState: #asynchronous Graph node, since Velocitas app works async
     action = state.get("decided_action")
 
     #based on the final verdict, set the responsive wiper mode
     if action == "STOP_WIPER":
-        await _vehicle_ref.Body.Windshield.Front.Wiping.Mode.set("OFF")
+        await vehicle_reference.Body.Windshield.Front.Wiping.Mode.set("OFF")
         msg = "Executed: Wiper.Mode = OFF via Velocitas SDK"
     elif action == "REDUCE_WIPER":
-        await _vehicle_ref.Body.Windshield.Front.Wiping.Mode.set("SLOW")
+        await vehicle_reference.Body.Windshield.Front.Wiping.Mode.set("SLOW")
         msg = "Executed: Wiper.Mode = SLOW"
     else:
         msg = "Executed: no change (KEEP_WIPER)"
